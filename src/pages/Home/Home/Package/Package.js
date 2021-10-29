302127495495a6cce4182727c3e4eac81cf43e50
mdshohidulislam1486 <Shohidul Islam>
2021-10-29 20:13:02 +0600

    updated

--- src/pages/Home/Home/Package/Package.js
+++ src/pages/Home/Home/Package/Package.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import { Button } from 'react-bootstrap';
 import { NavLink } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faShoppingCart, faShareAlt, faHeart} from '@fortawesome/free-solid-svg-icons'
+import './Packate.css'
 
 const Package = ({singlePackage}) => {
 
-    const {name, img, description} =singlePackage
+     
+    const {name, img, description, price} =singlePackage
     return (
         <div class="">
             <div class="col">
@@ -12,12 +16,16 @@ const Package = ({singlePackage}) => {
                     <img src={img} class="card-img-top" alt="tour pictures"/>
                     <div class="card-body">
                         <h5 class="card-title">{name}</h5>
+                        <p className='text-danger'>Single pax ${price}<small> US</small></p>
                         <p class="card-text">{description.slice(0, 200)}<NavLink className='text-decoration-none' to="/serviceDetails"> -see more</NavLink></p>
                     </div>
-                    <footer>
-                        <Button>
-                            Book Now
-                        </Button>
+                    <footer className='d-flex justify-content-around align-items-center mb-2'>
+                        <Button>Book Now</Button>
+                        <div className="text-warning cursor-custom">
+                            <FontAwesomeIcon className='me-2' icon={faShoppingCart}/>
+                            <FontAwesomeIcon className='me-2' icon={faShareAlt}/>
+                            <FontAwesomeIcon icon={faHeart}/>
+                        </div>
                     </footer>
                 </div>
            </div>
