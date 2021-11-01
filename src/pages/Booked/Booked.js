88d3f363f3ab530af7d5f4151db50eec8a2deff6
mdshohidulislam1486 <Shohidul Islam>
2021-11-01 22:21:16 +0600

    updated

--- src/pages/Booked/Booked.js
+++ src/pages/Booked/Booked.js
@@ -1,10 +1,35 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import useAuth from '../../hooks/useAuth';
 import './Booked.css'
 
 const Booked = () => {
+const {user}= useAuth()
+const [myBookings, setMyBookings] =useState([])
+const [myBooking, setMYBooking] =useState([])
+
+
+useEffect(()=>{
+    fetch('https://ghostly-crypt-13169.herokuapp.com/orders')
+    .then(res => res.json())
+    .then(data=> setMyBookings(data))
+},[])
+
+const myEmail = user.email
+
+const filterMyOrder = ()=>{
+
+}
+
+useEffect(()=>{
+    const updateBooking = myBookings.filter(myBooking => myBooking.email === myEmail )
+    setMYBooking(updateBooking)
+
+},[myBooking])
+
+
     return (
         <div className='custom-height'>
-            <h2>These services are booked alread</h2>
+            <h2>{myBooking.length}</h2>
         </div>
     );
 };
